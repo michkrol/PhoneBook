999b30e3903ae4c0f50c9e708e168080c94a61ea
Michał Król <michkrol@gmail.com>
2015-01-15 10:42:35 +0100

    Gotowe z dnia 15.01.2015r.

--- app/src/main/java/pl/edu/ug/aib/firstApp/RestBackgroundTask.java
+++ app/src/main/java/pl/edu/ug/aib/firstApp/RestBackgroundTask.java
@@ -8,6 +8,7 @@ import org.androidannotations.annotations.rest.RestService;
 
 import pl.edu.ug.aib.firstApp.data.Person;
 import pl.edu.ug.aib.firstApp.data.PhoneBook;
+import pl.edu.ug.aib.firstApp.data.Picture;
 
 @EBean
 public class RestBackgroundTask {
@@ -23,6 +24,13 @@ public class RestBackgroundTask {
         try {
             restClient.setHeader("X-Dreamfactory-Application-Name", "phonebook");
             PhoneBook phoneBook = restClient.getPhoneBook();
+            // pobranie dla każdego rekordu obrazka, jeśli jest przypisany
+            for (Person person: phoneBook.records) {
+                if (person.pictureId != null) {
+                    Picture picture = restClient.getPictureById(person.pictureId);
+                    person.pictureBytes = picture.base64bytes;
+                }
+            }
             publishResult(phoneBook);
         } catch (Exception e) {
             publishError(e);
--- app/src/main/java/pl/edu/ug/aib/firstApp/data/Person.java
+++ app/src/main/java/pl/edu/ug/aib/firstApp/data/Person.java
@@ -1,5 +1,6 @@
 package pl.edu.ug.aib.firstApp.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -8,6 +9,10 @@ public class Person {
     public String name;
     public String company;
     public String phone;
+    public Integer pictureId;
 
+    // atrybut istnieje w klasie, ale jest ignorowany przy zapytaniach JSONa
+    @JsonIgnore
+    public String pictureBytes;
 }
 
